39bd6594a5bedb6b9552e6faa4446ce25d420527
Sanskar <sanskarsoni300@gmail.com>
2022-11-29 19:58:05 +0530

    ADDED Massively -codedamn

---
+++ massively-landing-page/script.js
@@ -0,0 +1,9 @@
+function menuBar() {
+    document.querySelector(".menu-container").style.display = "block";
+    document.querySelector(".overlay").style.display = "block"
+}
+
+function closeMenu() {
+    document.querySelector(".menu-container").style.display = "none";
+    document.querySelector(".overlay").style.display = "none"
+}
