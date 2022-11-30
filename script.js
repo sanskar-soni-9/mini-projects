fbb24c5b5a3f81f8449cfec41d5b58432d0644e4
Sanskar <sanskarsoni300@gmail.com>
2022-11-30 14:38:36 +0530

    Mini Projects - Desktop Version done

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 "use strict";
 
 const projectsOverlay = document.querySelectorAll(".project-overlay");
+const navBar = document.querySelector("nav");
+const navLinks = document.querySelectorAll(".nav-link");
 
 //Event Handlers
+navBar.addEventListener("click", function (e) {
+  for (const link of navLinks) {
+    if (e.target === link) {
+      document
+        .querySelector(`.${link.dataset.target}`)
+        .scrollIntoView({ behavior: "smooth" });
+    }
+  }
+});
+
 window.addEventListener("mouseover", function (e) {
   for (const project of projectsOverlay) {
     if (e.target.closest(".project-overlay") === project) {
