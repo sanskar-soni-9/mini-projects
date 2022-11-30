09bce7bf913b2b94000080eb4cedd66f0aad40ac
Sanskar <sanskarsoni300@gmail.com>
2022-11-30 09:57:19 +0530

    Updated GTN - Fixed UI

--- Guess The Number/script.js
+++ Guess The Number/script.js
@@ -7,32 +7,31 @@ function randomNumber() {
 }
 randomNumber();
 
-document.querySelector(".check").addEventListener("click", function(){
+document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector("#user-input").value);
-  if(!guess) {
+  if (!guess) {
     document.querySelector(".status").textContent = "No number";
-  }
-  else if(guess === number) {
+  } else if (guess === number) {
     document.querySelector(".status").textContent = "Correct Answer";
     document.querySelector(".right-answer").textContent = number;
     document.querySelector("header").classList.add("won");
     document.querySelector("main").classList.add("won");
     document.querySelector("footer").classList.add("won");
-    document.querySelector("input").style.backgroundColor = "rgb(197, 194, 194)";
+    document.querySelector("input").style.backgroundColor =
+      "rgb(197, 194, 194)";
     document.querySelector("input").style.color = "rgb(40, 40, 40)";
     document.querySelector("input").style.borderColor = "rgb(197, 194, 194)";
-    if(score > highScore) {
+    if (score > highScore) {
       highScore = score;
       document.querySelector(".highscore").textContent = highScore;
     }
-  }
-  else {
-    if(score > 1) {
-      document.querySelector(".status").textContent = guess > number? "Too High!" : "Too Low!";
+  } else {
+    if (score > 1) {
+      document.querySelector(".status").textContent =
+        guess > number ? `${guess} is too High!` : `${guess} is too Low!`;
       score--;
       document.querySelector(".score").textContent = score;
-    }
-    else {
+    } else {
       document.querySelector(".status").textContent = "You Lost!!";
       score = 0;
       document.querySelector(".score").textContent = score;
@@ -40,7 +39,7 @@ document.querySelector(".check").addEventListener("click", function(){
   }
 });
 
-document.querySelector(".reset").addEventListener("click", function(){
+document.querySelector(".reset").addEventListener("click", function () {
   score = 20;
   document.querySelector(".score").textContent = score;
   document.querySelector(".status").textContent = "Start Guessing...";
